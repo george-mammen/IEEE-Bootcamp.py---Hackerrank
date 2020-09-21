5015f58e1502bd90fd32eb1b793c2c021c9b740e
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:44:38 +0530

    Update Smallest Element in List.py

--- Smallest Element in List.py
+++ Smallest Element in List.py
@@ -1,1 +1,26 @@
+"""
+Qs: Read a list and print the smallest element.
 
+Input Format
+First line contains n the number of elements in the list. The following n lines contains the elements of the list.
+
+Sample Input 0
+5
+8
+4
+7
+4
+5
+
+Sample Output 0
+4
+
+"""
+
+Code:
+numbers=[]
+n=int(input())
+for i in range(0,n):                      
+     elements = int(input())
+     numbers.append(elements) 
+print(min(numbers)) 
