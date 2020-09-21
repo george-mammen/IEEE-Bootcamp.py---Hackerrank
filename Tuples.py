9de9b2537f3c8f126754e736ffc040f08e36bab3
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:53:48 +0530

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,1 +1,19 @@
+"""
+Qs:Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of hash(t) .
 
+Sample Input 0
+2
+1 2
+
+Sample Output 0
+3713081631934410656
+
+"""
+Code:
+n=int(raw_input())
+a=raw_input()
+list=a.split()
+for i in xrange(n):
+        list[i] = int(list[i])
+my_tuple=tuple(list)        
+print hash(my_tuple) 
