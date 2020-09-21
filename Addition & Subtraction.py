bc69c5bf193cb0150b6cb2e840936f5ff2535a3c
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:36:47 +0530

    Update Addition & Subtraction.py

--- Addition & Subtraction.py
+++ Addition & Subtraction.py
@@ -1,1 +1,18 @@
+"""
+Qs: Read two numbers and print their sum and difference.
 
+Sample Input 0
+3
+2
+
+Sample Output 0
+5
+1
+"""
+
+
+Code :
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
