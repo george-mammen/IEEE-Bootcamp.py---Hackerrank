06579682067634e7a38743d461fbe74623bc5b3e
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:41:09 +0530

    Update N Odd Number.py

--- N Odd Number.py
+++ N Odd Number.py
@@ -1,1 +1,18 @@
+"""
+Qs:Read N from input and print the first N odd numbers
 
+Sample Input 0
+3
+
+Sample Output 0
+1
+3
+5
+
+"""
+
+Code :
+num=int(input(""))
+for i in range(1,num+num,2):
+    if((i % 2) ==1):
+        print("{0}".format(i)) 
