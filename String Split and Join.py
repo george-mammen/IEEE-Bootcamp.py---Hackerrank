dae7c2af93d6014c5d787ba2e1228873b2ddb9fc
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:51:43 +0530

    Update String Split and Join.py

--- String Split and Join.py
+++ String Split and Join.py
@@ -1,1 +1,15 @@
+"""
+Qs: You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
 
+Sample Input
+this is a string   
+
+Sample Output
+this-is-a-string
+
+"""
+Code:
+def split_and_join(line):
+    line=line.split()
+    line='-'.join(line)
+    return line
