1452c5bdf9f1fda430298dbe9b5d9b2fadf6c3d0
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 11:00:29 +0530

    Update Write a Function.py

--- Write a Function.py
+++ Write a Function.py
@@ -1,1 +1,20 @@
+"""
+Qs: Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+    Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 
+Sample Input 0
+1990
+
+Sample Output 0
+False
+
+"""
+Code:
+def is_leap(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
