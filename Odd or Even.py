e3579cd7a2015eab69447dcfb190a23e4d16df0b
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:39:01 +0530

    Update Odd or Even.py

--- Odd or Even.py
+++ Odd or Even.py
@@ -1,1 +1,20 @@
+"""
+Qs:Read a number and check if it is odd or even
 
+Sample Input 0
+5
+Sample Output 0
+odd
+
+Sample Input 1
+2
+Sample Output 1
+even
+"""
+
+Code:
+a=int(input())
+if (a % 2)==1:
+    print("odd")
+else:   
+    print("even")
