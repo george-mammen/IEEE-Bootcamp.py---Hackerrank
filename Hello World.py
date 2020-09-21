8bd510aa7fd9ff446b996b52fdf75305169bf27f
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-21 10:21:52 +0530

    Create Hello World.py

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+/*Program to print Hello World.
+
+Input Format:
+You do not need to read any input in this challenge.
+
+Output Format:
+Print Hello World
+
+Sample Output 0:
+Hello World */
+
+print('Hello World')
